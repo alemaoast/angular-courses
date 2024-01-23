6a6ddb5cb9c6f58095420510ccf0a4565d69b559
= <65097349+AndersonSiquelliTeofilo@users.noreply.github.com>
2024-01-23 20:54:29 -0300

    angular-blog: add fake data and page content

---
+++ angular-blog/src/app/data/dataFake.ts
@@ -0,0 +1,32 @@
+export const dataFake = [
+  {
+    "id": 1,
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWO7g7LMVqp1JEGBeaiBXDE4BBVkhJoUrDqIIBbnY_K3KwsMFPW_k_XdlM4a1eboHu4dI&usqp=CAU",
+    "title": "Saiu a nova versão do Angula",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+  },
+  {
+    "id": 2,
+    "photoCover": "https://devblogs.microsoft.com/typescript/wp-content/uploads/sites/11/2020/08/4.0-banner-image.png",
+    "title": "Announcing TypeScript 4.0 - TypeScript",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+  },
+  {
+    "id": 3,
+    "photoCover": "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    "title": "Python melhora sua versão, veja aqui.",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+  },
+  {
+    "id": 4,
+    "photoCover": "https://i.ytimg.com/vi/MxseaVcUd1M/maxresdefault.jpg",
+    "title": "JavaScript Versions - GeeksforGeeks",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+  },
+  {
+    "id": 5,
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqKjuhbeT0X2dID9PBl0ppOKG6hhzocblYx550s-MjiFg2_NRuc88J6rGzFQYOv_yKvRc&usqp=CAU",
+    "title": "Java News - Java News updated their cover photo.",
+    "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+  }
+];
--- angular-blog/src/app/pages/home/home.component.ts
+++ angular-blog/src/app/pages/home/home.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { dataFake } from 'src/app/data/dataFake'
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  data: Item[] = [];
+
+  ngOnInit(): void {
+    this.getDataFake();
+  }
+
+  getDataFake(): void {
+    this.data = dataFake.map(value => {
+      const item: Item = {
+        Id: value.id,
+        PhotoCover: value.photoCover,
+        Title: value.title,
+        Description: value.description
+      };
+      return item;
+    })
+  }
 
 }
+
+type Item = {
+  Id: number,
+  PhotoCover?: string,
+  Title?: string,
+  Description?: string
+};
