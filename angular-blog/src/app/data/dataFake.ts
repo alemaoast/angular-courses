7f6511c47f65aab06a57169e48646263e638b966
= <65097349+AndersonSiquelliTeofilo@users.noreply.github.com>
2024-01-23 21:47:47 -0300

    angular-blog: improving css

--- angular-blog/src/app/data/dataFake.ts
+++ angular-blog/src/app/data/dataFake.ts
@@ -4,29 +4,34 @@ export const dataFake = [
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWO7g7LMVqp1JEGBeaiBXDE4BBVkhJoUrDqIIBbnY_K3KwsMFPW_k_XdlM4a1eboHu4dI&usqp=CAU",
     "title": "Saiu a nova versão do Angula",
     "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    "showIn": ["big-card"],
   },
   {
     "id": 2,
     "photoCover": "https://devblogs.microsoft.com/typescript/wp-content/uploads/sites/11/2020/08/4.0-banner-image.png",
     "title": "Announcing TypeScript 4.0 - TypeScript",
     "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    "showIn": ["big-card", "small-card"],
   },
   {
     "id": 3,
     "photoCover": "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=1169&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "title": "Python melhora sua versão, veja aqui.",
     "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    "showIn": ["small-card"],
   },
   {
     "id": 4,
-    "photoCover": "https://i.ytimg.com/vi/MxseaVcUd1M/maxresdefault.jpg",
+    "photoCover": "https://images.unsplash.com/photo-1579468118864-1b9ea3c0db4a?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     "title": "JavaScript Versions - GeeksforGeeks",
     "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    "showIn": ["small-card"],
   },
   {
     "id": 5,
     "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqKjuhbeT0X2dID9PBl0ppOKG6hhzocblYx550s-MjiFg2_NRuc88J6rGzFQYOv_yKvRc&usqp=CAU",
     "title": "Java News - Java News updated their cover photo.",
     "description": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the.",
+    "showIn": ["small-card"],
   }
 ];
--- angular-blog/src/app/pages/home/home.component.ts
+++ angular-blog/src/app/pages/home/home.component.ts
@@ -1,35 +1,43 @@
 import { Component, OnInit } from '@angular/core';
 import { dataFake } from 'src/app/data/dataFake'
 
+type Item = {
+  Id: number,
+  PhotoCover?: string,
+  Title?: string,
+  Description?: string,
+  ShowIn?: string[]
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  data: Item[] = [];
+  dataBigCard: Item[] = [];
+  dataSmallCard: Item[] = [];
 
   ngOnInit(): void {
     this.getDataFake();
   }
 
   getDataFake(): void {
-    this.data = dataFake.map(value => {
+    this.dataBigCard = this.filterFakeData("big-card");
+    this.dataSmallCard = this.filterFakeData("small-card");
+  }
+
+  filterFakeData(showIn: string): Item[] {
+    return dataFake.filter(value => value.showIn.find(i => i == showIn)).map(value => {
       const item: Item = {
         Id: value.id,
         PhotoCover: value.photoCover,
         Title: value.title,
-        Description: value.description
+        Description: value.description,
+        ShowIn: value.showIn
       };
       return item;
-    })
+    });
   }
 
 }
-
-type Item = {
-  Id: number,
-  PhotoCover?: string,
-  Title?: string,
-  Description?: string
-};
